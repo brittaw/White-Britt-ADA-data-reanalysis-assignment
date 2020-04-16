e8937ab418017d042cc022d39994996093930cad
Britt A White <24brittwhite@gmail.com>
2020-04-16 18:43:45 -0400

    progress

--- White-Britt-ADA-data-reanalysis-assignment.R
+++ White-Britt-ADA-data-reanalysis-assignment.R
@@ -17,53 +17,61 @@ traits <- read_csv(df, col_names = TRUE)
 glimpse(traits)
 
 ###The data file they provided in Dryad contains 29 more species than the data table they produced so...
-m_sp_col <- read_csv("Mahler_species_col.csv", col_names = TRUE)
-traits_2 <- read_csv("Traits_2.csv", col_names = TRUE)
-traits_reduced <- m_sp_col %>% left_join(traits_2)
+#m_sp_col <- read_csv("Mahler_species_col.csv", col_names = TRUE)
+#traits_2 <- read_csv("Traits_2.csv", col_names = TRUE)
+#traits_reduced <- m_sp_col %>% left_join(traits_2)
 
 ###load NEXUS tree file - they specify this NEXUS tree in their README file 
 tree <- read.nexus("5Loc_25milburn_100kthin.MCC.median.nex")
 
-###The number of taxa in the data file and tree do not match
-tip_label <-tibble(tree$tip.label)
-
-dropped_tips <- tip_label %>% 
-anti_join(traits_reduced, c("tree$tip.label" = "species"))
-
-###the above gives a dataset of 27 tips to drop from an OG data set of 97, leaving 70. Two sp are now missing in the morph set.
-missing_dropped_tips <- traits_reduced %>% 
-  anti_join(tip_label, c("species" = "tree$tip.label"))
-
-###centralis and guazuma appear to not be in the tree files but are reported in the loadings table for the pPCA in the paper.
-###With this in mind I suppose I will continue with the analyses without the morph data for those species.
-
-#tree<-(drop.tip(tree, dropped_tips))
-#plot(tree)
-
 ###now we will remove missing data from the trait morph
-trait70sp <- traits_reduced %>% 
-semi_join(tip_label, c("species" = "tree$tip.label"))
+#trait70sp <- traits_reduced %>% 
+#semi_join(tip_label, c("species" = "tree$tip.label"))
 
 ###Nevermind, found this in supplements: 
   #"Thus, for our comparative analyses, measurements of A. guazuma correspond to A. garridoi in the
   #phylogeny, and measurements of A. centralis correspond to A. terueli in the phylogeny"
 traits_3 <- read_csv("Traits_3.csv", col_names = TRUE)
-traits_reduced <- m_sp_col %>% left_join(traits_3)
+tip_label <-tibble(tree$tip.label)
 trait72sp <- traits_3 %>% 
   semi_join(tip_label, c("species" = "tree$tip.label"))
+
+#get a list of taxa missing
+missing_tips <- tip_label %>% 
+  anti_join(trait72sp, c("tree$tip.label" = "species"))
+
+#create list
+miss <- c("Leiocephalus", "Polychrus", "acutus", "aeneus", "carpenteri", "ferreus", "fuscoauratus", 
+           "gingivinus", "griseus", "heterodermus", "leachii", "limifrons", "luciae", "marmoratus", 
+           "tandai", "ortonii", "pogus", "punctatus", "rejectus", "roquet", "schwartzi", "trachyderma", 
+           "transversalis", "trinitatis", "wattsii")
+
+##Now the tree has many species the data file does not
+tree<-(drop.tip(tree, miss))
+plot(tree)
 
 ###load necessary package for phylogenetic PCA
 library(phytools)
 
+#rename column sepcies to match tree
+trait72sp <- trait72sp %>% rename("tree$tip.label" = species)
+
 ###build the model of phylogenetic relationships to the covariance matrix of traits to determine if 
 ### covariance occurs at a rate greater than expected by Brownian Motion
-pPCA <- phyl.pca(tree, trait70sp, method="BM", mode="cov")
+
+pPCA <- phyl.pca(tree, trait72sp, method="BM", mode="cov")
 
 ##visualize in screeplot the PCAs
 screeplot(pPCA)
 scatter(pPCA, useLag=TRUE)
 plot(pPCA, useLag=TRUE)
 
-###Step 1. Calculate the Euclidean distance to every other species in a four-dimensional morphospace of traits.
+##Calculate the Euclidean distance to every other species in by the first four axes of a phylogenetic PCA on traits. 
+RePCA <- read_csv("Scores.retained.csv", col_names = TRUE)
+as_matrix(RePCA)
+library(philentropy)
 
-###Step 2. Test whether the new species forms a distinct phenotypic cluster with a specific clade (Chamaeleinides) by conducting a linear discriminant analysis (LDA) with 7 categories (1 represents the new species, 1 Chamaeleonides, then the remaining ecomorphs).  
+# compute the Euclidean Distance with default parameters
+distance(RePCA, method = "euclidean")
+
+##Conducted a 7-category linear discriminant analysis (LDA) on traditional ecomorph class for the four cryptic giants in our sample (A. landestoyi, A. chamaeleonides, A. guamuhaya, A. porcus) plus all species assigned to one of the six traditional ecomorph classes
